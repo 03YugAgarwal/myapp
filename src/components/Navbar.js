4c5dba54e3cdbf9ce91f9b3ac5821990e634c07f
Yug <03yugagarwal@gmail.com>
2022-06-27 16:18:28 +0530

    made navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./Navbar.css"
 import PropTypes from 'prop-types'
+
 // import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
+    const [active, setactive] = useState(false)
+
+    const handleClick=(event)=>{
+        setactive(active=>!active);
+    }
+
+    let toggleClassChec = active ? 'active': null;
+
     return (
         <>
             <nav className={`nav-${props.mode}`}>
                 <h1>{props.title}</h1>
-                <ul>
+                <button onClick={handleClick} href="#" className='toggle-button'>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </button>
+                <ul className={`${toggleClassChec}`}>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
                     <li><button onClick={props.toggleMode}>{props.mode}</button></li>
